f0dc391c285c78160e12fc7a579eaadcc665c4ff
smksapra2oke <smksapra2oke@gmail.com>
2026-02-21 19:01:41 +0700

    app.py

--- app.py
+++ app.py
@@ -8,22 +8,25 @@ model = joblib.load("model.pkl")
 encoders = joblib.load("encoders.pkl")
 target_encoder = joblib.load("target_encoder.pkl")
 
+features = [
+    "jurusan",
+    "nilai_ujikom",
+    "nilai_kejuruan",
+    "tempat_pkl_relevan",
+    "ekskul_aktif",
+    "status_tracer",
+    "bidang_pekerjaan",
+    "jabatan_pekerjaan",
+    "pendapatan"
+]
+
+@app.route("/")
+def home():
+    return "AI Random Forest running"
+
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
-
-    # urutan fitur HARUS sama
-    features = [
-        "jurusan",
-        "nilai_ujikom",
-        "nilai_kejuruan",
-        "tempat_pkl_relevan",
-        "ekskul_aktif",
-        "status_tracer",
-        "bidang_pekerjaan",
-        "jabatan_pekerjaan",
-        "pendapatan"
-    ]
 
     row = []
     for f in features:
@@ -32,7 +35,23 @@ def predict():
             value = encoders[f].transform([value])[0]
         row.append(value)
 
-    pred = model.predict([row])[0]
-    label = target_encoder.inverse_transform([pred])[0]
+    row = np.array(row).reshape(1, -1)
 
-    return jsonify({"prediction": label})
+    # prediksi label
+    pred_index = model.predict(row)[0]
+    pred_label = target_encoder.inverse_transform([pred_index])[0]
+
+    # probabilitas semua kelas
+    probs = model.predict_proba(row)[0]
+
+    class_labels = target_encoder.inverse_transform(range(len(probs)))
+
+    probability = {}
+    for label, prob in zip(class_labels, probs):
+        key = label.lower().replace(" ", "_")
+        probability[key] = float(prob)
+
+    return jsonify({
+        "prediction": pred_label,
+        "probability": probability
+    })
